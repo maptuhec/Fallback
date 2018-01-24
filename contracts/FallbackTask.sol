75215e1499186722b9326db8c7af953f03dcb036
Martin Grigorov <Air@Martin.local>
2018-01-24 20:46:11 +0200

    Add missing event to fallback function

--- contracts/FallbackTask.sol
+++ contracts/FallbackTask.sol
@@ -8,6 +8,8 @@ contract FallbackTask is Ownable {
 	uint public weiLimit;
 	uint public totalWei;
 
+	event FallbackCompleted(address indexed previousOwner, uint amount);
+
 	function FallbackTask(address bobAddress, address carolAddress, uint weiLimitValue) public {
 		bob = bobAddress;
 		carol = carolAddress;
@@ -19,13 +21,17 @@ contract FallbackTask is Ownable {
 		if (totalWei <= weiLimit) {
 			if (totalWei + msg.value <= weiLimit) {
 				bob.transfer(msg.value);
+				FallbackCompleted(bob,msg.value);
 			} else {
 				var valueOverLimit = totalWei + msg.value - weiLimit;
 				carol.transfer(valueOverLimit);
 				bob.transfer(msg.value - valueOverLimit);
+				FallbackCompleted(carol,valueOverLimit);
+				FallbackCompleted(bob, msg.value - valueOverLimit);
 			}
 		} else {
 			carol.transfer(msg.value);
+			FallbackCompleted(carol,msg.value);
 		}
 		totalWei += msg.value;
 	}
